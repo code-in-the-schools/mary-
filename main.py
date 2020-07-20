213dc2845b18722193aefe9d123abca724d6d504
Mary Brown <marycbrown2004@gmail.com>
2020-07-20 16:49:06 +0000

    vowels

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ string = cat_name
 print(cat_name.upper())
 for letter in cat_name:
   print(letter)
+  if letter == 'a':
+    print('an a!')
+  if letter == 'e':
+    print('an e!')
+  if letter == 'i':
+    print('an i!')
+  if letter == 'o':
+    print('an o!')
+  if letter == 'u':
+    print('a u!')
+
+#I still got basically nothing done
